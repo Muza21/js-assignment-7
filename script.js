4600e73278c14eba755b7114fcf4abf8a9b208e9
Muza21 <lasha.muzashvili1@gmail.com>
2025-03-17 23:43:46 +0400

    style: add html and style

---
+++ script.js
@@ -0,0 +1,5 @@
+// H/A #7
+
+// 1) In the center of the page there is a button that says Click me! When you hover over it with a 50% probability, it disappears and appears in a random place. When clicked, it does the same with 100% probability.
+
+// 2) The page consists of two halves: a stopwatch and a timer. The functionality is appropriate, you can receive data for the timer simply by text input, or you can use buttons like “+1m.”. Accurate down to seconds.
